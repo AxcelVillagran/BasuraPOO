1a27785b6d11a176cdd8ed883c063a06db8d3ad8
Axcel Villagran <avglatina1@outlook.com>
2023-06-24 19:07:11 -0500

    Sout llamen a dios

--- PruebaFideo/src/main/java/avg/pruebafideo/PruebaFideo.java
+++ PruebaFideo/src/main/java/avg/pruebafideo/PruebaFideo.java
@@ -13,5 +13,7 @@ public class PruebaFideo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Llamen a dios");
+        
     }
 }
